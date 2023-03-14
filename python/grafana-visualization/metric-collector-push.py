0ab5f3e13c7f620fce12b4129c4ccd658fcb2e27
mkibyte <kmurali.87@gmail.com>
2023-03-14 20:26:55 +0530

    adding modifications

--- python/grafana-visualization/metric-collector-push.py
+++ python/grafana-visualization/metric-collector-push.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python
 
+#!/usr/bin/python
 import requests
 import time
 import os
@@ -7,10 +7,9 @@ from sonarqube import SonarCloudClient
 from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
 
 # SonarQube API endpoint
-#SONARQUBE_API_ENDPOINT = 'http://localhost:9000/api/measures/component?component=sam-alpha-uno&metricKeys=coverage&additionalFields=metrics'
 SONARCLOUD_URL = 'http://localhost:9000/'
+
 # SonarQube Token
-#SONARQUBE_TOKEN = "sqp_d1c95b943ec072c25906eb4432047eb541e0384f"
 SONARCLOUD_TOKEN = "squ_c06649c70def5a44e8aceb52f5d9de999fec5dac"
 
 # Prometheus Pushgateway endpoint
@@ -23,7 +22,7 @@ SONARQUBE_METRIC = 'coverage'
 SONARQUBE_COMPONENT_KEY = 'sam-alpha-uno'
 
 # Prometheus metric name
-PROMETHEUS_METRIC_NAME = 'sam_alpha_uno_coverage'
+PROMETHEUS_METRIC_NAME = 'coverage'
 
 # Main function
 def main():
@@ -33,22 +32,17 @@ def main():
         print('SQ-metric-value :', sonarqube_metric_value)
         # Push the metric to Prometheus Pushgateway
         push_to_prometheus_pushgateway(PROMETHEUS_METRIC_NAME, sonarqube_metric_value)
-
         # Wait for 5 minutes before collecting the metric again
         time.sleep(300)
 
 # Function to get the metric value from SonarQube
 def get_sonarqube_metric_value(metric, component_key):
-    # Make the API request to SonarQube
-    #sonarqube_api_url = SONARQUBE_API_ENDPOINT.format(component=component_key, metric=metric)
-    #response = requests.get(sonarqube_api_url, auth=('<user-name>', '<password>'))
-    # Parse the API response and get the metric value
-    #response_json = response.json()
-    #metric_value = response_json['component']['measures'][0]['value']
     sonar = SonarCloudClient(sonarcloud_url=SONARCLOUD_URL,token=SONARCLOUD_TOKEN)
-    component = sonar.measures.get_component_with_specified_measures(component=SONARQUBE_COMPONENT_KEY,fields="metrics,periods",metricKeys="coverage")
+    component = sonar.measures.get_component_with_specified_measures(
+        component=SONARQUBE_COMPONENT_KEY,
+        fields="metrics,periods",
+        metricKeys="coverage")
     measures = component['component']['measures']
-
     for metric_data in measures:
         print(metric_data['metric'])
         if metric_data['metric'] == 'coverage':
@@ -62,10 +56,8 @@ def push_to_prometheus_pushgateway(metric_name, metric_value):
     # Create a Prometheus Gauge metric
     registry = CollectorRegistry()
     gauge_metric = Gauge(metric_name, 'Sam Alpha UNO project Coverage', registry=registry)
-
     # Set the metric value
     gauge_metric.set(metric_value)
-
     # Push the metric to the Pushgateway
     push_to_gateway(PROMETHEUS_PUSHGATEWAY_ENDPOINT, job='sam_uno_job', registry=registry)
 
